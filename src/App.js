8cbd00d803f93741bcc4b60af8d84195e8163183
nblake707 <33844526+nblake707@users.noreply.github.com>
2021-11-29 16:02:21 -0500

    conditionally render gallery

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/about';
-import Nav from './components/Nav'
+import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
+
+  // lifted up the state from the nav component - this allows us to pass this info as props to the nav component 
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    {
+      name: 'portraits', 
+      description: 'Portraits of people in my life'
+    },
+    {
+      name: 'food',
+      description: 'Delicious delicacies'
+    },
+    {
+      name: 'landscape',
+      description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
+    }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav />
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      />
       <main>
-        <About />
+        <div>
+          <Gallery />
+          <About />
+        </div>
       </main>
     </div>
   );
